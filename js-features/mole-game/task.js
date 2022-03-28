af261abbeac0ce72367fe8a27cfd8203c9d03b13
Dmitry_home <79811924115@yandex.ru>
2022-03-28 04:04:09 +0300

    mole v.2

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,29 +1,28 @@
-let kill = document.getElementById('dead')
-let miss = document.getElementById('lost')
+let kill = document.getElementById("dead");
+let miss = document.getElementById("lost");
 
-// hole1.onclick = () => {
-//   if(hole1.className.includes('hole_has-mole')){
-//   kill.textContent++
-//   } else {
-//     miss.textContent++
-//   }
-// }
-
-  getHole = index => document.getElementById(`hole${index}`)
-  for (let index = 1; index <= 9; index++) {
-    getHole( index ).onclick = () => {
-    if(getHole( index ).className.includes('hole_has-mole')){
-      kill.textContent++
+getHole = (index) => document.getElementById(`hole${index}`);
+for (let index = 1; index <= 9; index++) {
+  getHole(index).onclick = () => {
+    if (getHole(index).className.includes("hole_has-mole")) {
+      kill.textContent++;
+      if (kill.textContent == 10) {
+        setTimeout(() => {
+          alert("Победа!");
+          kill.textContent = 0;
+          miss.textContent = 0;
+        }, 100);
+      }
     } else {
-      miss.textContent++
+      miss.textContent++;
+      if (miss.textContent == 5) {
+        setTimeout(() => {
+          alert("Проиграл!");
+          kill.textContent = 0;
+          miss.textContent = 0;
+        }, 100);
       }
     }
-  } 
-
-
-// console.log(getHole(1).className) // Так работает 
-
-// for (let index = 0; index < 9; index++) {
-//   // console.log(getHole(index).className) // evaluating 'getHole(index).className')???
-// }
-  
+  };
+}
+// Игрок побеждает, если своевременно убивает 10 кротов. При 5 поражениях игра заканчивается.
