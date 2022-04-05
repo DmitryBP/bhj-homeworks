85671cca88221d2b3833072d88351260f3bbf273
Dmitry_home <79811924115@yandex.ru>
2022-04-05 19:58:04 +0300

    element-search v.3

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -2,15 +2,15 @@
 let modal1 = document.getElementById('modal_main')
 let modal2 = document.getElementById('modal_success')
 let xx = Array.from(document.querySelectorAll('.modal__close')) 
-// let xx = document.querySelectorAll('.modal__close') ? Так тоже работает 
 let button = document.getElementsByClassName('btn')
 
 modal1.classList.add('modal_active')
 
 //Удаление попапа
+
 xx.forEach(x => {
-  x.onclick = (event) => {
-    currentModal = event.target.closest('.modal_active') // Вы писали про This, я не понял где его использовать
+  x.onclick = function() {
+    currentModal = this.closest('.modal_active') 
     currentModal.classList.remove('modal_active')
   }
 });
@@ -20,8 +20,3 @@ button[0].onclick = () => {
   modal1.classList.remove('modal_active')
   modal2.classList.add('modal_active')
 }
-
-// В первом задании у вас по клику на крестик добавляется обработчик события на каждый элемент крестика.
-//  Вам нужно сделать универсальный обработчик кликов по крестику. 
-//  Вы можете с помощью цикла перебирать все элементы крестиков и на них добавлять обработчик события, 
-// а внутри обработчика события с помощью контекста вызова (this) в котором находится объект на котором произошло событие с помощью closest находить то модальное окно к которому принадлежит крестик и его закрывать.
--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -3,40 +3,65 @@ let right = document.querySelector(".slider__arrow_next");
 let images = Array.from(document.querySelectorAll(".slider__item"));
 let i = 0;
 
-let rightFunction = () => {
-  if (i < images.length - 1) {
-    images[i].classList.remove("slider__item_active");
-    images[i + 1].classList.add("slider__item_active");
-    i++;
+// let rightFunction = () => {
+//   if (i < images.length - 1) {
+//     images[i].classList.remove("slider__item_active");
+//     images[i + 1].classList.add("slider__item_active");
+//     i++;
+//   } else {
+//     images[i].classList.remove("slider__item_active");
+//     i = 0;
+//     images[i].classList.add("slider__item_active");
+//   }
+// };
+
+// let leftFunction = () => {
+//   console.log(`test111 ${i}`);
+//   if (i > 0) {
+//     images[i].classList.remove("slider__item_active");
+//     images[i - 1].classList.add("slider__item_active");
+//     i--;
+//     console.log(`if left ${i}`);
+//   } else {
+//     images[images.length - 1].classList.remove("slider__item_active");
+//     i = images.length - 1;
+//     images[i].classList.add("slider__item_active");
+//     console.log(`else left ${i}`);
+//   }
+// };
+
+// right.onclick = rightFunction;
+
+// left.onclick = leftFunction;
+// console.log(`test ${i}`);
+
+//Вторая версиясв 
+
+let toglLeft = () => {
+  activSlaid = images.findIndex((item) =>
+    item.classList.contains("slider__item_active")
+  );
+  images[activSlaid].classList.remove("slider__item_active");
+  if (activSlaid == 0) {
+    images[4].classList.add("slider__item_active");
   } else {
-    images[i].classList.remove("slider__item_active");
-    i = 0;
-    images[i].classList.add("slider__item_active");
+    images[activSlaid - 1].classList.add("slider__item_active");
   }
 };
-
-let leftFunction = () => {
-  console.log(`test111 ${i}`);
-  if (i > 0) {
-    images[i].classList.remove("slider__item_active");
-    images[i - 1].classList.add("slider__item_active");
-    i--;
-    console.log(`if left ${i}`);
+let toglRight = () => {
+  activSlaid = images.findIndex((item) =>
+    item.classList.contains("slider__item_active")
+  );
+  images[activSlaid].classList.remove("slider__item_active");
+  if (activSlaid == 4) {
+    images[0].classList.add("slider__item_active");
   } else {
-    images[images.length - 1].classList.remove("slider__item_active");
-    i = images.length - 1;
-    images[i].classList.add("slider__item_active");
-    console.log(`else left ${i}`);
+    images[activSlaid + 1].classList.add("slider__item_active");
   }
 };
-
-right.onclick = rightFunction;
-
-left.onclick = leftFunction;
-console.log(`test ${i}`);
-
-
-// test = images.findIndex(function(item) {               // не получается найти позицию активного слайда
-//   item.classList.contains("slider__item_active")
-// })
-// console.log(test)
+left.onclick = () => {
+  toglLeft();
+};
+right.onclick = () => {
+  toglRight()
+}
