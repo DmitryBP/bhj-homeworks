3a82aaf9d6ab4d228ff07eeaa713ddf30a963d87
Dmitry_home <79811924115@yandex.ru>
2022-04-12 22:59:48 +0300

    dom v.1

---
+++ dom/ads/task.js
@@ -0,0 +1,25 @@
+let ads = Array.from(document.querySelectorAll('.rotator__case'))
+let i = 0
+
+
+console.log(ads[0].dataset)
+
+setInterval(() => {
+  
+  if (i < ads.length-1) {
+    ads[i].classList.remove('rotator__case_active')
+    ads[i+1].classList.add('rotator__case_active')
+    ads[i+1].style.color = ads[i+1].dataset.color 
+    i++
+  } else {
+    ads[i].classList.remove('rotator__case_active')
+    i = 0
+    ads[i].classList.add('rotator__case_active')
+  }
+  let speed = ads[i].dataset.speed  // ??? не разобрался как передать speed в параметр скорости settimout
+  
+  console.log (speed)
+}, 1000 )
+
+
+
