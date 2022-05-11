ddd57b7103ac14887ccafbac8e197e43a1b95922
Dmitry_home <79811924115@yandex.ru>
2022-05-11 19:39:34 +0300

    document structure v3.1

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,20 +1,52 @@
-let links = Array.from(document.getElementsByClassName('has-tooltip'))
-
-
-
-links.forEach(link => {
-  link.onclick = () => {
-    console.log(link.nextElementSibling.className !== 'tooltip')
-    if (link.nextElementSibling.className !== 'tooltip') {
-      link.insertAdjacentHTML('afterend', `
-      <div class="tooltip" style="left: 0; top: 0">
-      ${'test'}
-      </div>
-      `)
+let links = document.querySelectorAll('.has-tooltip')
+links.forEach((link, index) => {
+    let flag = false
+    link.onclick = () => {
+        let position = link.getBoundingClientRect()
+        let dataPosition = link.getAttribute('data-position')
+ 
+        if (!flag) {
+            if(dataPosition == 'bottom'){
+                link.insertAdjacentHTML('afterend', `
+                <div class="tooltip" style="left: ${position.x}px; top: ${position.bottom}px; position: absolute">
+                ${link.title}
+                </div>
+                `)
+            }
+            if(dataPosition == 'top'){
+                link.insertAdjacentHTML('afterend', `
+                <div class="tooltip" style="left: ${position.x}px; top: ${position.top-30}px; position: absolute">
+                ${link.title}
+                </div>
+                `)
+            }
+            if(dataPosition == 'right'){
+                link.insertAdjacentHTML('afterend', `
+                <div class="tooltip" style="left: ${position.x + position.width}px; top: ${position.top-30}px; position: absolute">
+                ${link.title}
+                </div>
+                `)
+            }
+            if(dataPosition == 'left'){
+                link.insertAdjacentHTML('afterend', `
+                <div class="tooltip" style="left: ${position.x - position.width}px; top: ${position.top-30}px; position: absolute">
+                ${link.title}
+                </div>
+                `)
+            }
+            flag = true
+        }
+ 
+        link.nextElementSibling.classList.toggle('tooltip_active')
+ 
+        let activTips = document.querySelectorAll('.tooltip_active')
+        activTips.forEach(activTip => {
+            if(activTip !== links[index].nextElementSibling){
+                activTip.classList.remove('tooltip_active')
+            }
+        });
+        
+        return false
     }
-    // if(link.nextElementSibling)
-
-
-    return false
-  }
 });
+ 
