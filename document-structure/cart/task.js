a4050c347ff788e11681f22a1b913dec27018fcb
Dmitry_home <79811924115@yandex.ru>
2022-05-11 19:27:53 +0300

    document structure v3

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -1,57 +1,74 @@
-const productQuantityValue = document.querySelectorAll(
-  ".product__quantity-value"
-);
-const productQuantityDec = document.querySelectorAll(
-  ".product__quantity-control_dec"
-);
-const productQuantityInc = document.querySelectorAll(
-  ".product__quantity-control_inc"
-);
-const cartBtn = document.querySelectorAll(".product__add");
-const cart = document.querySelector(".cart__products");
-const img = document.querySelectorAll(".product__image");
+const controlDec = document.querySelectorAll('.product__quantity-control_dec')
+const controlInc = document.querySelectorAll('.product__quantity-control_inc')
+const addToCart = document.querySelectorAll('.product__add')
+const cart = document.querySelector('.cart__products')
+const cartActive = document.querySelector('.cart')
 
-productQuantityInc.forEach((el, i) => {
-  el.onclick = () => {
-    productQuantityValue[i].textContent++;
-  };
-});
 
-productQuantityDec.forEach((el, i) => {
-  el.onclick = () => {
-    if (productQuantityValue[i].textContent > 1)
-      productQuantityValue[i].textContent--;
-  };
-});
-let arr = [];
-cartBtn.forEach((el, i) => {
-  el.onclick = () => {
-    if (!arr.includes(String(i + 1))) {
-      cart.insertAdjacentHTML(
-        "afterbegin",
-        `
-      <div class="cart__product" data-id="${el
-        .closest(".product")
-        .getAttribute("data-id")}">
-      <img class="cart__product-image" src="${img[i].src}">
-      <div class="cart__product-count">${
-        productQuantityValue[i].textContent
-      }</div>
-      </div>
-      `
-      );
-      arr.push(el.closest(".product").getAttribute("data-id"));
-    } else {
-      const cartProductCount = document.querySelectorAll(
-        ".cart__product-count"
-      );
-      cartProductCount.forEach(el => {
-        if (el.closest(".cart__product").getAttribute("data-id") == i + 1) {
-          let a = Number(el.textContent);
-          let b = Number(productQuantityValue[i].textContent);
-          el.textContent = a + b;
-        }
-      });
+
+controlDec.forEach(dec => {
+  dec.onclick = () => {
+    if(dec.nextElementSibling.textContent > 1) {
+      dec.nextElementSibling.textContent--
     }
-  };
+  }
+})
+
+controlInc.forEach(inc => {
+  inc.onclick = () => {
+    inc.previousElementSibling.textContent++
+  }
 });
+
+addToCart.forEach(addButton => {
+  addButton.onclick = () => {
+    
+    let addedProductId = addButton.closest('.product').getAttribute('data-id')
+    let addedProductValue = Number(addButton.previousElementSibling.children[1].textContent)
+    let addedProductImgPosition = addButton.closest('.product').children[1].getBoundingClientRect()
+
+    
+    let cartProducts = Array.from(cart.children)
+    
+    let findInCart = cartProducts.find(prod=> {
+      return prod.getAttribute('data-id') == addedProductId
+    })
+    
+    let addedProductIndex = cartProducts.indexOf(findInCart)
+    
+    if(cartProducts[addedProductIndex]){
+      let howMachInCart = Number(cartProducts[addedProductIndex].children[1].textContent) 
+      cartProducts[addedProductIndex].children[1].textContent = howMachInCart + addedProductValue
+
+      const cartProductImgPosition = cartProducts[addedProductIndex].children[0].getBoundingClientRect()
+      
+      console.log('cart.x', cartProductImgPosition.x)
+      console.log('cart.y', cartProductImgPosition.y)
+    }
+    
+    if (!findInCart) {
+      cart.insertAdjacentHTML('afterbegin', `
+      <div class="cart__product" data-id="${addButton.closest('.product').getAttribute('data-id')}">
+      <img class="cart__product-image" src="${addButton.closest('.product__controls').previousElementSibling.src}">
+      <div class="cart__product-count">${addButton.previousElementSibling.children[1].textContent}</div>
+      <button class="cart__delite-product">Удалить</button>
+      </div>
+      `)
+    }
+    
+    cartActive.classList.add('cart-active')
+    
+    const cartProductsDelite = cart.querySelector('.cart__delite-product')
+    
+    cartProductsDelite.onclick = () => {
+      cartProductsDelite.closest('.cart__product').remove()
+      let arrCart = document.getElementsByClassName('cart__product')
+      if(arrCart.length == 0){
+        cartActive.classList.remove('cart-active')
+      }
+    }
+    
+    
+  }
+})
+
--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,51 +1,60 @@
-const input = document.querySelector('.tasks__input')
-const form = document.querySelector('.tasks__control')
-const taskList = document.querySelector('.tasks__list')
-const data = JSON.parse(localStorage.getItem('items'))
-const clinBtn = document.querySelector('.clin')
+const input = document.querySelector(".tasks__input");
+const form = document.querySelector(".tasks__control");
+const taskList = document.querySelector(".tasks__list");
+const data = JSON.parse(localStorage.getItem("items"));
+const clinBtn = document.querySelector(".clin");
 
-let arrItems 
-localStorage.getItem('items') ? arrItems = JSON.parse(localStorage.getItem('items')) : arrItems = []
+let arrItems = localStorage.getItem("items")
+  ? JSON.parse(localStorage.getItem("items"))
+  : [];
 
 form.onsubmit = () => {
-  return false
-}
+  return false;
+};
 
-let addItem = text => {
-  taskList.insertAdjacentHTML('afterbegin', `
-  <div class="task">
-  <div class="task__title">
-  ${text}
-  </div>
-  <a href="#" class="task__remove">&times;</a>
-  </div>
-  `)
-  // input.value = ''
-}
+let addItem = (text) => {
+  taskList.insertAdjacentHTML(
+    "afterbegin",
+    `<div class="task"><div class="task__title">${text}</div><a href="#" class="task__remove">&times;</a></div>`
+  );
+};
+
+/* Если вы добавляете задачу в самое начало, 
+то добавляйте обработчик события только на самый первый элемент крестика */
+/* Видимо я не правильно понял Вашу идею  */
+
+/* let taskDelete = () => {
+  const AllDellButton = taskList.querySelectorAll('.task__remove')
+  AllDellButton[0].onclick = () => {
+    AllDellButton[0].closest('.task').remove()
+    arrItems.splice(0, 1)
+    localStorage.setItem('items', JSON.stringify(arrItems))
+    }
+} */
 
 let taskDelete = () => {
-  const xx = taskList.querySelectorAll('.task__remove')
-  xx.forEach((x, i) => {
-    x.onclick = () => {
-      arrItems.splice(i, 1)
-      x.closest('.task').remove()
-      localStorage.setItem('items', JSON.stringify(arrItems))
+  taskList.onclick = (e) => {
+    if (e.target.innerText == "×") {
+      // ?? скопировал x из консоли, с моим x из клавиатуры не работало
+      e.target.closest(".task").remove();
+      let delitedItem = arrItems.indexOf(e.target.previousSibling.innerText);
+      arrItems.splice(delitedItem, 1);
+      localStorage.setItem("items", JSON.stringify(arrItems));
+      console.log(arrItems);
     }
-  });
-}
+  };
+};
 
 input.onchange = () => {
-  taskDelete()
-  
-  addItem(input.value)
+  addItem(input.value);
+  arrItems.push(input.value);
+  input.value = "";
+  localStorage.setItem("items", JSON.stringify(arrItems));
+  taskDelete();
+};
 
-  arrItems.push(input.value)
-
-  localStorage.setItem('items', JSON.stringify(arrItems))
-}
-
-data.forEach(item => {
-  addItem(item)
+data.forEach((item) => {
+  addItem(item);
 });
 
-taskDelete()
+taskDelete();
